76f3d274c05e9d161a530dfbdbe5dd049bdc31be
Sayed Mohsen <samohsen97@gmail.com>
2020-11-26 03:53:42 +0200

    update Hw

---
+++ common/Bit_Math.h
@@ -0,0 +1,37 @@
+ /******************************************************************************
+ *
+ * Module: Common - Macros
+ *
+ * File Name: bit_math.h
+ *
+ * Description: Commonly used Macros
+ *
+ * Author: Sayed Mohsen
+ *
+ *******************************************************************************/
+
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+
+#include "STD_Types.h"
+
+
+/* Set a certain bit in any register */
+#define SET_BIT(REG,BIT) (REG|=(1<<BIT))
+
+/* Clear a certain bit in any register */
+#define CLR_BIT(REG,BIT) (REG&=(~(1<<BIT)))
+
+/* Toggle a certain bit in any register */
+#define TOGGLE_BIT(REG,BIT) (REG^=(1<<BIT))
+
+/* Read a certain bit in any register */
+#define READ_BIT(REG,BIT) ((REG>>BIT) & 0X01)
+
+/* Check if a specific bit is set in any register and return true if yes */
+#define BIT_IS_SET(REG,BIT) ( REG & (1<<BIT) )
+
+/* Check if a specific bit is cleared in any register and return true if yes */
+#define BIT_IS_CLEAR(REG,BIT) ( !(REG & (1<<BIT)) )
+
+#endif
--- common/tm4c123gh6pm_hw.h
+++ common/tm4c123gh6pm_hw.h
@@ -127,4 +127,62 @@ typedef union
 /**********************************************************************************************************************
  *   END System Control HW
  *********************************************************************************************************************/
+
+
+
+
+
+/*===========================================================*
+ * GPIO REGISTERS                                            *
+ *========================================================== */
+typedef struct
+{
+    uint32 GPIODATA    ;
+    uint32 GPIODIR     ;
+    uint32 GPIOIS      ;
+    uint32 GPIOIBE     ;
+    uint32 GPIOIEV     ;
+    uint32 GPIOIM      ;
+    uint32 GPIORIS     ;
+    uint32 GPIOMIS     ;
+    uint32 GPIOICR     ;
+      uint32 GPIOAFSEL   ; /*offset : 0x420+4 byte = 0x424 end:0x424*/
+
+    uint8 Reserved[0xDC];/*500-424 = DC byte 0r 37 uint32 */
+    
+    uint32 GPIODR2R    ; /*offset : 0x500 */
+    uint32 GPIODR4R    ;
+    uint32 GPIODR8R    ;
+    uint32 GPIOODR     ;
+    uint32 GPIOPUR     ;
+    uint32 GPIOPDR     ;
+    uint32 GPIOSLR     ;
+    uint32 GPIODEN     ;
+    uint32 GPIOLOCK    ;
+    uint32 GPIOCR      ;
+    uint32 GPIOAMSEL   ;
+    uint32 GPIOPCTL    ;
+    uint32 GPIOADCCTL  ;
+    uint32 GPIODMACTL  ;
+}PORT_Registers;
+
+#define PORTA_REG     (*((volatile PORT_Registers*)0x400043FC))
+#define PORTB_REG     (*((volatile PORT_Registers*)0x400053FC))
+#define PORTC_REG     (*((volatile PORT_Registers*)0x400063FC))
+#define PORTD_REG     (*((volatile PORT_Registers*)0x400073FC))
+#define PORTE_REG     (*((volatile PORT_Registers*)0x400243FC))
+#define PORTF_REG     (*((volatile PORT_Registers*)0x400253FC))
+
+
+
+
+
+
+
+
+
+
+
+
+
 #endif
